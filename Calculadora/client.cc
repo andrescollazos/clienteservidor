f3a34ffd2e64f6323c316f70d7aeb965fb88369f
andrescollazos <pipecollazos1@gmail.com>
2017-02-09 00:29:11 -0500

    Calculadora: Terminada. El servidor puede realizar las operaciones de suma, resta, multiplicación, división, calcular el factorial de un numero, saber si un número es primo o no

--- Calculadora/client.cc
+++ Calculadora/client.cc
@@ -7,17 +7,56 @@ using namespace std;
 using namespace zmqpp;
 
 int main() {
-	cout << "This is the client\n";
+	cout << "CALCULADORA\n";
 
 	context ctx;
 	socket s(ctx, socket_type::req);
 
-	cout << "Connecting to tcp port 5555\n";
+	//cout << "Connecting to tcp port 5555\n";
 	s.connect("tcp://localhost:5555");
 
-	cout << "Sending a hello message!\n";
+	cout << "Tipo de operaciones: \n";
+	cout << "\t1. + a b : Suma a con b\n";
+	cout << "\t2. - a b : Resta a con b\n";
+	cout << "\t3. * a b : Multiplica a con b\n";
+	cout << "\t4. / a b : Divide a entre b\n";
+	cout << "\t5. f a : Factorial de a\n";
+	cout << "\t6. p a : a es primo? \n";
+
+	int opcion = 0;
+	int a, b;
+
+	while (not (opcion >= 1 && opcion <= 6)) {
+		cout << "\nSeleccione una de las opciones: ";
+		cin >> opcion;
+	}
+
+	cout << "\n Digite a: ";
+	cin >> a;
+	if (not(opcion >= 5)) {
+		cout << "\n Digite b: ";
+		cin >> b;
+	}
+
 	message m;
-	m << "+" << 300000 << 5000000;
+	if (opcion == 1) {
+		m << "+" << a << b;
+	}
+	else if (opcion == 2) {
+		m << "-" << a << b;
+	}
+	else if (opcion == 3) {
+		m << "*" << a << b;
+	}
+	else if (opcion == 4) {
+		m << "/" << a << b;
+	}
+	else if (opcion == 5) {
+		m << "f" << a << 0;
+	}
+	else if (opcion == 6) {
+		m << "p" << a << 0;
+	}
 
 	s.send(m);
 
@@ -26,7 +65,7 @@ int main() {
 
 	int result;
 	answer >> result;
-	cout << "answer: " << result << endl;
+	cout << "Resultado: " << result << endl;
 
 
 	//int i;
--- Calculadora/server.cc
+++ Calculadora/server.cc
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <string>
 #include <zmqpp/zmqpp.hpp>
+#include <math.h>
 
 using namespace std;
 using namespace zmqpp;
 
+int factorial(int n){
+	int result = 1;
+	for (int i = n; i > 0; i--) {
+			result *= i;
+	}
+	return result;
+}
+
+int es_primo(int n) {
+	int divisores = 2;
+
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			divisores++;
+		}
+	}
+	if (divisores == 2) return 1;
+	return 0;
+}
+
 int main() {
-	cout << "This is the server\n";
+	cout << "SERVIDOR ARITMETICO\n";
 
 	context ctx;
 	socket s(ctx, socket_type::rep);
 
-	cout << "Binding socket to tcp port 5555\n";
+	cout << "[SERVER DICE] : Binding socket to tcp port 5555\n";
 	s.bind("tcp://*:5555");
 
 	while (true) {
@@ -19,21 +40,37 @@ int main() {
 		s.receive(m);
 
 		string operation;
-		int arg1, arg2;
+		int arg1, arg2, result;
 
 		m >> operation >> arg1 >> arg2;
-		int result = arg1 + arg2;
+		if (operation == "+") {
+			result = arg1 + arg2;
+		}
+		else if (operation == "-") {
+			result = arg1 - arg2;
+		}
+		else if (operation == "*") {
+			result = arg1 * arg2;
+		}
+		else if (operation == "/") {
+			result = arg1 / arg2;
+		}
+		else if (operation == "f") {
+			result = factorial(arg1);
+		}
+		else if (operation == "p") {
+			result = es_primo(arg1);
+		}
 
 		// Procesando el mensaje
-		cout << "Received " << arg1 << " " << operation << " "<< arg2 << " = " << result << endl;
+		cout << "[SERVER DICE] : Received " << arg1 << " " << operation << " "<< arg2 << " = " << result << endl;
 
 		message response;
 		response << result;
 
 		s.send(response);
-
-
-		//cout << "Finished\n";
-		return 0;
 	}
+
+	//cout << "Finished\n";
+	return 0;
 }
