33ebb083e13d80a4e1e9f35f81910ba6d10b37fd
andrescollazos <pipecollazos1@gmail.com>
2017-02-22 00:22:31 -0500

    Servidor: Subir Archivos. El usuario puede subir un archivo al servidor de ficheros.

--- servidor de archivos/client.cc
+++ servidor de archivos/client.cc
@@ -7,26 +7,49 @@
 using namespace std;
 using namespace zmqpp;
 
-/**
- * Writes the raw data contained in msg to a file on disk with name fileName.
- *
- * Assumptions:
- * - The message consists of only one part.
- * - That part is binary written to disk
- */
-void messageToFile(const message& msg, const string& fileName) {
-	const void *data;
-	msg.get(&data, 0);
-	size_t size = msg.size(0);
+// Funcion que permite leer los bytes de un archivo y retornarlos en un vector
+vector<char> readFileToBytes(const string& fileName) {
+	// ios::binary - > Abrir el archivo en modo binario
+	// ios::ate -> Posicionar el puntero al final del archivo
+	ifstream ifs(fileName, ios::binary | ios::ate);
+	ifstream::pos_type pos = ifs.tellg(); // Conocer la cantidad de bytes del arch.
 
+	vector<char> result(pos);
+
+	// Retornar el puntero a la primera posicion
+	ifs.seekg(0, ios::beg);
+	ifs.read(result.data(), pos);
+
+	return result;
+}
+
+ // Convertir un fichero a un mensaje para enviar
+void fileToMesage(const string& fileName, message& msg) {
+	// Convertir un archivo en un vector de bytes
+	vector<char> bytes = readFileToBytes(fileName);
+	msg.add_raw(bytes.data(), bytes.size());
+}
+
+ // Convertir un mensaje a un fichero
+void messageToFile(const message& msg, const string& fileName) {
+	// Tener apuntar a un espacio en memoria donde estaran los datos
+	const void *data;
+	msg.get(&data, 0); // Obtener el primer elemento del mensaje
+	size_t size = msg.size(0); // Calcular los bytes
+
+	// Crear un archivo, que se trabajara de manera binaria
 	ofstream ofs(fileName, ios::binary);
+	// Escribir los bytes en el archivo
 	ofs.write((char*)data, size);
 }
 
 int main(int argc, char** argv) {
-	cout << "Filetransfer example\n";
-	if (argc != 2) {
-		cerr << "Must be called: " << argv[0] << " file\n";
+	cout << "SERVIDOR DE TRANSFERENCIA DE ARCHIVOS" << endl;
+	// argv[1] -> Nombre del archivo
+	// argv[2] -> Operacion a realizar: up - down - list - remove
+
+	if (argc != 3) {
+		cerr << "Must be called: " << argv[0] << " file operation\n";
 		return 1;
 	}
 
@@ -34,18 +57,44 @@ int main(int argc, char** argv) {
 	socket s(ctx, socket_type::req);
 	s.connect("tcp://localhost:5555");
 
-	// Request the file in argv[1]
+	// Nombre del archivo
 	string fileName(argv[1]);
 	message m;
-	m << fileName;
+	m << argv[2] << fileName;
 	s.send(m);
 
-	message response;
-	s.receive(response);
+	// Casos de uso
 
-	// response contains the file, write it to disk
-	string downloadName("down-" + fileName);
-	messageToFile(response, downloadName);
-    cout << "Finished\n";
+	// Descargar archivo:
+	if ((string)argv[2] == "down") {
+		message file;
+		cout << "[SERVER]: Recibiendo Archivo ..." << endl;
+		s.receive(file);
+		cout << "[SERVER]: Archivo recibido ..." << endl;
+
+		// File contiene los bytes que seran escritos
+		string downloadName("down-" + fileName); // Cambiar de nombre
+		cout << "[SERVER]: Sincronizando archivo ..." << endl;
+		messageToFile(file, downloadName);
+		cout << "[SERVER]: Operacion terminada ..." << endl;
+
+	}
+	else if ((string)argv[2] == "up") {
+		message response;
+		s.receive(response);
+		string ack;
+		response >> ack;
+
+		if (ack == "f") {
+			cout << "[SERVER]: Proceso de subir archivo autorizado ..." << endl;
+			message file;
+			// Convertir archivo a un mensaje
+			fileToMesage(fileName, file);
+			cout << "[SERVER]: Subiendo archivo ..." << endl;
+			s.send(file);
+			cout << "[SERVER]: Archivo subido ..." << endl;
+		}
+	}
+
 	return 0;
 }
--- servidor de archivos/server.cc
+++ servidor de archivos/server.cc
@@ -6,42 +6,86 @@
 using namespace std;
 using namespace zmqpp;
 
-
+// Funcion que permite leer los bytes de un archivo y retornarlos en un vector
 vector<char> readFileToBytes(const string& fileName) {
+	// ios::binary - > Abrir el archivo en modo binario
+	// ios::ate -> Posicionar el puntero al final del archivo
 	ifstream ifs(fileName, ios::binary | ios::ate);
-	ifstream::pos_type pos = ifs.tellg();
+	ifstream::pos_type pos = ifs.tellg(); // Conocer la cantidad de bytes del arch.
 
 	vector<char> result(pos);
 
+	// Retornar el puntero a la primera posicion
 	ifs.seekg(0, ios::beg);
 	ifs.read(result.data(), pos);
 
 	return result;
 }
 
+// Convertir un fichero a un mensaje para enviar
 void fileToMesage(const string& fileName, message& msg) {
+	// Convertir un archivo en un vector de bytes
 	vector<char> bytes = readFileToBytes(fileName);
 	msg.add_raw(bytes.data(), bytes.size());
 }
 
+// Convertir un mensaje a un fichero
+void messageToFile(const message& msg, const string& fileName) {
+	// Tener apuntar a un espacio en memoria donde estaran los datos
+	const void *data;
+	msg.get(&data, 0); // Obtener el primer elemento del mensaje
+	size_t size = msg.size(0); // Calcular los bytes
+
+	// Crear un archivo, que se trabajara de manera binaria
+	ofstream ofs(fileName, ios::binary);
+	// Escribir los bytes en el archivo
+	ofs.write((char*)data, size);
+}
+
 int main() {
-	cout << "This is the server\n";
+	cout << "SERVIDOR\n";
 
 	context ctx;
 	socket s(ctx, socket_type::rep);
 	s.bind("tcp://*:5555");
 
+	// Recibir primer mensaje:
+	// op - filename
+	// op: operacion a realizar up - down - list - remove
+	// filename: nombre del archivo.
 	message m;
-	s.receive(m);
+	string fileName, op;
+	
+	while (true) {
+		s.receive(m);
+		m >> op >> fileName;
 
-	string fileName;
-	m >> fileName;
+		// Saber que operacion va a realizar el usuario
 
-	message response;
-	// m contains the filename requested by the client.
-	fileToMesage(fileName,response);
-	s.send(response);
+		// Descargar archivo
+		if (op == "down") {
+			cout << "[SERVER]: Solicitud de Descarga: " << fileName << endl;
+			message response;
+			fileToMesage(fileName, response);
+			s.send(response);
+			cout << "[SERVER]: Descarga Realizada" << endl;
+		}
+		// Subir archivo
+		else if (op == "up") {
+			// El servidor envia un mensaje "f",
+			// que significa que el usuario puede enviar los bytes
+			s.send("f");
 
+			// Se prepara un mensaje para recibir los bytes
+			message file;
+			s.receive(file);
+			cout << "[SERVER]: Solicitud de subida: " << fileName << endl;
+
+			string upName("up-" + fileName);
+			messageToFile(file, upName);
+			cout << "[SERVER]: Subida Realizada" << endl;
+		}
+	}
 	cout << "Finished\n";
 	return 0;
 }
