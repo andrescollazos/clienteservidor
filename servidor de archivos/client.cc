f9dd23774fd4764a63177b9e2afbafc6ae4c11fd
andrescollazos <pipecollazos1@gmail.com>
2017-02-24 17:28:15 -0500

    Servidor: Aunteticación El servidor solicita nombre de usuario y contraseña al cliente que se conecta, este provee esta información, si el usuario no existe lo crea. Y permite realizar las funciones de subir, descargar y eliminar archivos. Nota: Se presenta un problema cuando se vuelve a abrir el server y lee el archivo .users.dat que contiene la información de los usuarios... Linea 87 en server.cc

--- servidor de archivos/client.cc
+++ servidor de archivos/client.cc
@@ -49,63 +49,126 @@ int main(int argc, char** argv) {
 	// argv[2] -> Operacion a realizar: up - down - list - remove
 
 	if (argc != 3) {
-		cerr << "Must be called: " << argv[0] << " file operation\n";
+		cerr << "Error: " << argv[0] << " debe especificar username y password\n";
 		return 1;
 	}
 
 	context ctx;
 	socket s(ctx, socket_type::req);
 	s.connect("tcp://localhost:5555");
+	bool finish = false;
+	string aut;
+	string userName(argv[1]);
+	string password(argv[2]);
 
-	// Nombre del archivo
-	string fileName(argv[1]);
-	message m;
-	m << argv[2] << fileName;
-	s.send(m);
+	while (not(finish)) {
+		cout << "Iniciando autenticacion" << endl;
+		message auth;
+		auth << userName << password;
+		s.send(auth);
 
-	// Casos de uso
+		s.receive(auth);
+		auth >> aut;
 
-	// Descargar archivo:
-	if ((string)argv[2] == "down") {
-		message file;
-		cout << "[SERVER]: Recibiendo Archivo ..." << endl;
-		s.receive(file);
-		cout << "[SERVER]: Archivo recibido ..." << endl;
+		if (aut == "authenticated") {
+			// Nombre del archivo
+			int opcion = 0;
+			while(not(opcion >= 1 && opcion <= 5)) {
+				cout << "OPCIONES:" << endl;
+				cout << "\t1. Subir Archivo" << endl;
+				cout << "\t2. Descargar Archivo" << endl;
+				cout << "\t3. Eliminar Archivo" << endl;
+				cout << "\t4. Listar Archivos" << endl;
+				cout << "\t5. Salir" << endl;
+				cout << "DIGITE UNA OPCION: ";
+				cin >> opcion;
+			}
 
-		// File contiene los bytes que seran escritos
-		string downloadName("down-" + fileName); // Cambiar de nombre
-		cout << "[SERVER]: Sincronizando archivo ..." << endl;
-		messageToFile(file, downloadName);
-		cout << "[SERVER]: Operacion terminada ..." << endl;
+			string fileName;
+			message m;
 
-	}
-	else if ((string)argv[2] == "up") {
-		message response;
-		s.receive(response);
-		string ack;
-		response >> ack;
+			// Casos de uso
 
-		if (ack == "f") {
-			cout << "[SERVER]: Proceso de subir archivo autorizado ..." << endl;
-			message file;
-			// Convertir archivo a un mensaje
-			fileToMesage(fileName, file);
-			cout << "[SERVER]: Subiendo archivo ..." << endl;
-			s.send(file);
+			// Descargar archivo:
+			if (opcion == 2) { //((string)argv[2] == "down") {
+				cout << "\n\t DESCARGAR ARCHIVOS" << endl;
+				cout << "Digite el nombre del archivo (con su extension): ";
+				cin >> fileName;
+				m << "down" << fileName;
+				s.send(m);
 
-			s.receive(response);
-			response >> ack;
-			cout << "[SERVER]: Archivo subido: " << ack << endl;
+				message file;
+				cout << "[SERVER]: Recibiendo Archivo ..." << endl;
+				s.receive(file);
+				cout << "[SERVER]: Archivo recibido ..." << endl;
+
+				// File contiene los bytes que seran escritos
+				string downloadName("down-" + fileName); // Cambiar de nombre
+				cout << "[SERVER]: Sincronizando archivo ..." << endl;
+				messageToFile(file, downloadName);
+				cout << "[SERVER]: Operacion terminada ..." << endl;
+
+			}
+			else if (opcion == 1) {//((string)argv[2] == "up") {
+				cout << "\n\t SUBIR ARCHIVOS" << endl;
+				cout << "Digite el nombre del archivo (con su extension): ";
+				cin >> fileName;
+				m << "up" << fileName;
+				s.send(m);
+
+				message response;
+				s.receive(response);
+				string ack;
+				response >> ack;
+
+				if (ack == "f") {
+					cout << "[SERVER]: Proceso de subir archivo autorizado ..." << endl;
+					message file;
+					// Convertir archivo a un mensaje
+					fileToMesage(fileName, file);
+					cout << "[SERVER]: Subiendo archivo ..." << endl;
+					s.send(file);
+
+					s.receive(response);
+					response >> ack;
+					cout << "[SERVER]: Archivo subido: " << ack << endl;
+				}
+			}
+			else if (opcion == 3) {//((string)argv[2] == "rm") {
+				cout << "\n\t ELIMINAR ARCHIVOS" << endl;
+				cout << "Digite el nombre del archivo (con su extension): ";
+				cin >> fileName;
+				m << "rm" << fileName;
+				s.send(m);
+
+				cout << "[SERVER]: Procediendo a eliminar archivo ..." << endl;
+				message response;
+				s.receive(response);
+				string result;
+				response >> result;
+
+				cout << "[SERVER]: Proceso completado: " << result << " ..." << endl;
+			}
+			else if (opcion == 4) {
+				cout << "\n\t LISTAR ARCHIVOS" << endl;
+				m << "list" << "";
+				//s.send(m);
+			}
+			else if (opcion == 5) {
+				cout << "TERMINANDO SESIÓN ..." << endl;
+				message fin;
+				fin << "finish" << "";
+				s.send(fin);
+				finish = true;
+			}
 		}
-	}
-	else if ((string)argv[2] == "rm") {
-		cout << "[SERVER]: Procediendo a eliminar archivo ..." << endl;
-		message response;
-		s.receive(response);
-		string result;
-		response >> result;
-
-		cout << "[SERVER]: Proceso completado: " << result << " ..." << endl;
+		else if (aut == "password-err") {
+			cout << "[SERVER]: Contraseña ingresada es incorrecta!" << endl;
+			finish = true;
+		}
+		else if (aut == "bad-authenticated") {
+			cout << "[SERVER]: Usuario NO existe, se procede a crearlo! " << endl;
+		}
 	}
 
 	return 0;
--- servidor de archivos/server.cc
+++ servidor de archivos/server.cc
@@ -6,6 +6,31 @@
 using namespace std;
 using namespace zmqpp;
 
+// Clase Usuario
+class user {
+	public:
+		string name;
+		string password;
+		vector<string> archives;
+
+		void reg(string nom, string pass) {
+			name = nom;
+			password = pass;
+		}
+		void add_arch(string arch) {
+			archives.push_back(arch);
+		}
+
+		void view() {
+			std::cout << "Nombre: " << name << endl;
+			std::cout << "Archivos: " << endl;
+			int sze = (int)archives.size();
+			for(int i = 0; i < sze ; i++) {
+				std::cout << "\t "<< (i+1) << ". " << archives[i] << endl;
+			}
+		}
+};
+
 // Funcion que permite leer los bytes de un archivo y retornarlos en un vector
 vector<char> readFileToBytes(const string& fileName) {
 	// ios::binary - > Abrir el archivo en modo binario
@@ -43,61 +68,137 @@ void messageToFile(const message& msg, const string& fileName) {
 }
 
 int main() {
-	cout << "SERVIDOR\n";
+	// Realizar operaciones sobre los archivos directamente es mas lento, que
+	// cargarlos a memoria, procesarlos y una vez terminado ese proceso, guardarlos
+	vector<user> users; // Vector que contiene la informacion en tiempo de ejecucion
+	user useri; // Contiene la informacion de un usuario i
+	int n_users = 0; // Numero de usuarios
+	ifstream if_users_arch(".users.dat", ios::in | ios::binary);
+	// El archivo debe existir, si no se crea
+	if (if_users_arch.good()) {
+		cout << "[SERVER]: Cargando datos de usuarios ..." << endl;
+		if_users_arch.seekg(0, ios::end);
+		ifstream::pos_type pos = if_users_arch.tellg();
+		if_users_arch.seekg(0, ios::beg);
+		n_users = ((int)pos)/((int)sizeof(user));
+		cout << "[SERVER]: Cantidad de usuarios " << n_users << endl;
+		for(int i = 0; i < n_users; i++) {
+			cout << "\tCargando Usuario: ";
+			if_users_arch.read(reinterpret_cast<char *>(&useri), sizeof(user));
+			cout << useri.name << " ..." << endl;
+			users.push_back(useri);
+		}
+		int a;
+		cout << "a: ";
+		cin >> a;
+		cout << "AL MENOS NO ENTRO" << endl;
+		if_users_arch.close();
+		cout << "[SERVER]: Datos cargados correctamente ..." << endl;
+	}
+	ofstream of_users_arch(".users.dat", ios::out | ios::binary);
+
+	//fsalida.write(reinterpret_cast<char *>(&andres), sizeof(user));
+
+	cout << "[SERVER]: Esperando peticiones ..." << endl;
 
 	context ctx;
 	socket s(ctx, socket_type::rep);
 	s.bind("tcp://*:5555");
 
-	// Recibir primer mensaje:
-	// op - filename
-	// op: operacion a realizar up - down - list - remove
-	// filename: nombre del archivo.
-	message m;
-	string fileName, op;
-
+	// El servidor recibe la conexion de un usuario con nombre y contrasena
+	// En caso de que el servidor no encuentre el usuario en los registros
+	// le informara que no existe el usuario, que si desea crearlo.
 	while (true) {
+		// Recibir primer mensaje:
+		// op - filename
+		// op: operacion a realizar up - down - list - remove
+		// filename: nombre del archivo.
+		message m;
+		string fileName, op;
+		string username, password;
+		bool exists = false;
+		bool auth;
+
+		// Proceso de autenticacion:
 		s.receive(m);
-		m >> op >> fileName;
-
-		// Saber que operacion va a realizar el usuario
-
-		// Descargar archivo
-		if (op == "down") {
-			cout << "[SERVER]: Solicitud de Descarga: " << fileName << endl;
-			message response;
-			fileToMesage(fileName, response);
-			s.send(response);
-			cout << "[SERVER]: Descarga Realizada" << endl;
-		}
-		// Subir archivo
-		else if (op == "up") {
-			// El servidor envia un mensaje "f",
-			// que significa que el usuario puede enviar los bytes
-			s.send("f");
-
-			// Se prepara un mensaje para recibir los bytes
-			message file;
-			s.receive(file);
-			cout << "[SERVER]: Solicitud de subida: " << fileName << endl;
-
-			string upName("up-" + fileName);
-			messageToFile(file, upName);
-			cout << "[SERVER]: Subida Realizada" << endl;
-			s.send("correctamente"); // Se subio el archivo correctamente
-		}
-		else if (op == "rm") {
-			cout << "[SERVER]: Solicitud de Borrado" << endl;
-			string remov = "up-" + fileName;
-			// La funcion remove() recibe const char, no string
-			const char * rem = remov.c_str(); // Conversion string -> const char
-			if (remove(rem)!= 0) {
-				cout << "[SERVER]: Borrado Incorrecto" << endl;
-				s.send("Incorrecto");
+		m >> username >> password;
+		// Comprobar que el usuario exista.
+		for (int i = 0; i < n_users; i++) {
+			if (users[i].name == username) {
+				// El usuario existe, debemos comprar la contrasena
+				exists = true;
+				if (users[i].password == password) {
+					cout << "[SERVER]: autenticacion terminada" << endl;
+					s.send("authenticated");
+					auth = true;
+				}
+				else {
+					cout << "[SERVER]: Contrasenia incorrecta" << endl;
+					s.send("password-err");
+					auth = false;
+				}
 			}
-			else {
-				cout << "[SERVER]: Borrado Correctamente" << endl;
-				s.send("Correctamente");
+		}
+		if (not(exists)) {
+			// EL usuario no existe, entonces se crea:
+			cout << "[SERVER]: Usuario no exite, se procede a crearlo" << endl;
+			s.send("bad-authenticated");
+			useri.reg(username, password);
+			users.push_back(useri);
+			of_users_arch.write(reinterpret_cast<char *>(&useri), sizeof(user));
+			n_users++;
+			auth = false;
+		}
+
+		if (auth) {
+			cout << "[SERVER]: Usuario autenticado-- Puede iniciar peticiones " << endl;
+			s.receive(m);
+			m >> op >> fileName;
+
+			// Saber que operacion va a realizar el usuario
+
+			// Descargar archivo
+			if (op == "down") {
+				cout << "[SERVER]: Solicitud de Descarga: " << fileName << endl;
+				message response;
+				fileToMesage(fileName, response);
+				s.send(response);
+				cout << "[SERVER]: Descarga Realizada" << endl;
+			}
+			// Subir archivo
+			else if (op == "up") {
+				// El servidor envia un mensaje "f",
+				// que significa que el usuario puede enviar los bytes
+				s.send("f");
+
+				// Se prepara un mensaje para recibir los bytes
+				message file;
+				s.receive(file);
+				cout << "[SERVER]: Solicitud de subida: " << fileName << endl;
+
+				string upName("up-" + fileName);
+				messageToFile(file, upName);
+				cout << "[SERVER]: Subida Realizada" << endl;
+				s.send("correctamente"); // Se subio el archivo correctamente
+			}
+			else if (op == "rm") {
+				cout << "[SERVER]: Solicitud de Borrado" << endl;
+				string remov = "up-" + fileName;
+				// La funcion remove() recibe const char, no string
+				const char * rem = remov.c_str(); // Conversion string -> const char
+				if (remove(rem)!= 0) {
+					cout << "[SERVER]: Borrado Incorrecto" << endl;
+					s.send("Incorrecto");
+				}
+				else {
+					cout << "[SERVER]: Borrado Correctamente" << endl;
+					s.send("Correctamente");
+				}
+			}
+			else if (op == "finish") {
+				cout << "[SERVER]: Terminar!" << endl;
+				of_users_arch.close();
+				break;
 			}
 		}
 	}
